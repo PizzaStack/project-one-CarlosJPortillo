cca37f362fd0ae6eeb07aa6945f071669a2c2d9c
Carlos J Portillo <36525286+CarlosJPortillo@users.noreply.github.com>
2019-01-09 01:08:11 -0600

    Delete login.js

--- client_side/login.js
+++
@@ -1,8 +0,0 @@
-function validateInfomration(){
-    let userName = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
-
-    if(userName.length == 0 || password.length){
-        alert("You have the username and/or password field as empty");
-   }
-}
